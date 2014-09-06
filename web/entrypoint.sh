fb0f579f1482ba3396255b36c52506d71ef7fbcc
Nathan LeClaire <nathan.leclaire@docker.com>
2014-09-05 17:52:18 -0700

    Poll with nc instead of waiting for 10 seconds

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Stalling for DB"
-sleep 10
+echo "Stalling for redis & mysql"
+while true; do
+    nc -q 1 $REDIS_1_PORT_6379_TCP_ADDR 6379 >/dev/null && break
+done
+while true; do
+    nc -q 1 $DB_1_PORT_3306_TCP_ADDR 3306 >/dev/null && break
+done
 echo "Starting web container"
 cd /laravel
 php artisan migrate --force
