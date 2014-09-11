482c02f6ebbd06d04a23cec98e944182693a0be3
Nathan LeClaire <nathan.leclaire@docker.com>
2014-09-11 14:04:48 -0700

    Make Disney change

--- web/laravel/app/views/pages/home.blade.php
+++ web/laravel/app/views/pages/home.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
 <div class="row">
-   <h1>Hey there meetup!</h1>
+   <h1>Hey there Disney!</h1>
 </div>
 <div class="row">
    <a href="/users">Click here to see the users.</a>
-    <p>hello classmates</p>
 </div>
 @stop
